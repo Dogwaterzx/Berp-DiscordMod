f1b0d68761664c35c36bfc2b01150c778e32cdef
XGNFox <94414099+XGNFox@users.noreply.github.com>
2022-07-08 01:20:08 -0400

    Update deploy-commands.js
    yes

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -4,8 +4,9 @@ const { Routes } = require('discord-api-types/v9');
 const { CLIENTID, GUILDID, TOKEN } = require('../config.json');
 
 const commands = [
-	new SlashCommandBuilder().setName('whitelist').setDescription('Whitelist Players From device ban.').addStringOption((option) => option.setName('gamertag').setDescription('The player you want to whitelist via their Gamertag').setRequired(true)),
-	new SlashCommandBuilder().setName('unwhitelist').setDescription('Removes Gamertag From Whitelist').addStringOption((option) => option.setName('gamertag').setDescription('The Gamertag u Want to Remove From The Whitelist').setRequired(true)),
+    new SlashCommandBuilder().setName('list-players').setDescription('Gets a list of people currently on classic.'),
+	new SlashCommandBuilder().setName('realm-whitelist').setDescription('Whitelist Players From device ban.').addStringOption((option) => option.setName('gamertag').setDescription('The player you want to whitelist via their gamertag').setRequired(true)),
+	new SlashCommandBuilder().setName('realm-unwhitelist').setDescription('Removes gamertag From Whitelist').addStringOption((option) => option.setName('gamertag').setDescription('The gamertag u Want to Remove From The Whitelist').setRequired(true)),
 
 ].map(command => command.toJSON());
 
